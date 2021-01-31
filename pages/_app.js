f7e685161346db41d8ca1b670aa8e96726ab548d
Kira <priyanshushrama709@gmail.com>
2021-01-31 23:13:58 +0530

    add blog structure - components folder, posts and css things

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Layout from "../components/Layout";
+import Alert from "../components/Alert"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Alert>
+      <Layout home={true}>
+        <Component {...pageProps} />
+      </Layout>
+    </Alert>
+
+  )
 }
 
 export default MyApp
--- pages/about.js
+++
@@ -1,5 +0,0 @@
-const about = () => (
-    <h1>This is about page</h1>
-)
-
-export default about;
