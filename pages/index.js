4e6cb0ed7ae7944d7a562c91345ef526a1c9cadb
Kira <priyanshushrama709@gmail.com>
2021-02-01 21:23:47 +0530

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import fs from 'fs'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
 // import { getSortedPostsData } from "../lib/posts";
 import path from 'path'
